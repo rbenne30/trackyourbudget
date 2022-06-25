f575621f49b8f1f57e879768419ac5de6f4975f6
rbenne30 <ryannebennett1@outlook.com>
2022-06-25 14:55:46 -0400

    update repo

---
+++ public/js/idb.js
@@ -0,0 +1,39 @@
+// create variable to hold db connection
+const indexedDB = window.indexedDB;
+let db;
+// establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
+const request = indexedDB.open('track_yourbudget', 1);
+// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+request.onupgradeneeded = function(event) {
+    // save a reference to the database 
+    const db = event.target.result;
+    // create an object store (table) called `new_entry`, set it to have an auto incrementing primary key of sorts 
+    db.createObjectStore('new_entry', { autoIncrement: true });
+  };
+//   // upon a successful 
+// request.onsuccess = function(event) {
+//     // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+//     db = event.target.result;
+  
+//     // check if app is online, if yes run uploadEntry() function to send all local db data to api
+//     if (navigator.onLine) {
+//       // we haven't created this yet, but we will soon, so let's comment it out for now
+//       // uploadEntry();
+//     }
+//   };
+  
+//   request.onerror = function(event) {
+//     // log error here
+//     console.log(event.target.errorCode);
+//   };
+// This function will be executed if we attempt to submit a new entry and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_entry'], 'readwrite');
+  
+    // access the object store for `new_entry`
+    const entryObjectStore = transaction.objectStore('new_entry');
+  
+    // add record to your store with add method
+    entryObjectStore.add(record);
+  }
